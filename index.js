bf8893a9b82d0835084bb92f5e0152381cd2f89c
Miguel Saraiva <miguelsaraiva1999@gmail.com>
2024-02-15 00:42:56 +0000

    added availabel and play

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const httpServer = require('http').createServer()
 const io = require("socket.io")(httpServer, {
     cors: {
     // The origin is the same as the Vue app domain. Change if necessary
-        origin: "http://127.0.0.1:5173", 
         methods: ["GET", "POST"], 
         credentials: true
     } 
@@ -10,6 +9,22 @@ const io = require("socket.io")(httpServer, {
 
 httpServer.listen(8080, () =>{ console.log('listening on *:8080')})
 
+let playersWaiting = []
+
 io.on('connection', (socket) => {
-    console.log(`client ${socket.id} has connected`) 
+    console.log(`client ${socket.id} has connected`)
+    socket.on('available' , (data) => {
+        if(playersWaiting.length > 0) {
+            const opponent = playersWaiting.pop()
+            socket.data.opponent = opponent
+            io.to(opponent).emit('startGame', {opponent: data.name, id: socket.id, firstPlayer: true})
+            socket.emit('startGame', {opponent: data.name, id: opponent, firstPlayer: true})
+        } else {
+            playersWaiting.push(socket.id)
+        }
+    });
+
+    socket.on('play', (data) => {
+        io.to(socket.data.opponent).emit('play', {cell: data.cell, position: data.position})
+    })
 })
